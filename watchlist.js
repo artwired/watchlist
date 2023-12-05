5f6d64d3b72951eea5cdc56fdc0f571a3ab02005
Brad Cole <artwired@users.noreply.github.com>
2023-12-05 11:11:59 -0800

    added find movies on watchlist page
    When there are no movies in the watchlist the watchlist informs the user and prompts them to find some movies to add to their watchlist

--- watchlist.js
+++ watchlist.js
@@ -1,13 +1,19 @@
 const watchlistText = document.getElementById("watchlist-text");
 const watchlistHolder = document.getElementById("watchlist-holder");
-watchlistText.textContent = "test 1, 2, 3";
 
 let output = ``;
-let movieItem = JSON.parse(localStorage.getItem("imdbMovie")) || [];
+let movieItem = JSON.parse(localStorage.getItem("imdbMovie"));
 
-for (let movie of movieItem) {
-  output += `
+if (movieItem) {
+  for (let movie of movieItem) {
+    output += `
     <h1>${movie.Title}</h1>
   `;
+  }
+  watchlistHolder.innerHTML = output;
+} else {
+  watchlistHolder.innerHTML = `
+  <p class="watchlist-text" id="watchlist-text">Your watchlist is looking a little empty...</p>
+  <a href="./index.html" class="find-movies-btn">Let's add some movies!</a>
+  `;
 }
-watchlistHolder.innerHTML = output;
